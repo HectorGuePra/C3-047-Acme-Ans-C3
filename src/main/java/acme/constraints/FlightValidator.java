44e56b081a8e6c0f652884bd5ef63c6170b6391f
JoaquinBorjaLeon <joaborleo@alum.us.es>
2025-05-16 16:53:37 +0200

    cost validator for flight

--- src/main/java/acme/constraints/FlightValidator.java
+++ src/main/java/acme/constraints/FlightValidator.java
@@ -49,6 +49,9 @@ public class FlightValidator extends AbstractValidator<ValidFlight, Flight> {
 				}
 		}
 
+		if (flight.getCost() != null)
+			super.state(context, flight.getCost().getCurrency().equals("EUR"), "cost", "acme.validation.flight.cost-euro.message");
+
 		result = !super.hasErrors(context);
 		return result;
 	}
