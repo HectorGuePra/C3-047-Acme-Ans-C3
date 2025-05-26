38be0f4cf0965b34626a0fc488cbf574a61d36c6
DLJ7666 <jarguezduran@gmail.com>
2025-05-26 22:57:25 +0200

    correccion de error de limites (otra vez)

--- src/main/java/acme/entities/tasks/Task.java
+++ src/main/java/acme/entities/tasks/Task.java
@@ -24,7 +24,7 @@ import lombok.Setter;
 	})
 public class Task extends AbstractEntity {
 
-	private static final long	serialVersionUID	= 1L;
+	private static final long	serialVersionUID	=  1L;
 
 	@Mandatory
 	@Valid
